a620097c78d9d863159aaf97488e7afe2d97ca1f
Mitchell Davis <mitchell.peter.davis@gmail.com>
2016-05-10 20:48:37 +1000

    Testing issues with includes.

--- app/API/V1/Controllers/InspectionController.php
+++ app/API/V1/Controllers/InspectionController.php
@@ -5,11 +5,15 @@ namespace App\API\V1\Controllers;
 use App\API\V1\Repositories\InspectionRepository as Repository;
 use App\API\V1\Transformers\InspectionTransformer as Transformer;
 use Illuminate\Support\Collection;
+use Illuminate\Http\Request;
 
 class InspectionController extends APIController
 {
-	public function getList(Repository $repository)
+	public function getList(Repository $repository, Request $request)
 	{
+		var_dump($request->input('include'));
+		exit;
+
 		$items = $repository->findAll();
 
 		return $this->response->collection(Collection::make($items), new Transformer());
