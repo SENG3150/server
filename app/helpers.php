6c3f8ef63a69abb764cfff3337d169ddf5ed3504
Mitchell Davis <mitchell.peter.davis@gmail.com>
2016-05-29 00:43:18 +1000

    Added bounding checks for helper functions.

--- app/helpers.php
+++ app/helpers.php
@@ -15,3 +15,78 @@ if(function_exists('entity_dump') == FALSE)
 		echo '</pre>';
 	}
 }
+
+if(function_exists('generateRoutes') == FALSE)
+{
+	/**
+	 * @param Dingo\Api\Routing\Router $api
+	 * @param string                   $version
+	 * @param string                   $route
+	 * @param string                   $controller
+	 * @param bool                     $getList
+	 * @param bool                     $get
+	 * @param bool                     $create
+	 * @param bool                     $update
+	 * @param bool                     $delete
+	 */
+	function generateRoutes($api, $version, $route, $controller = NULL, $getList = TRUE, $get = TRUE, $create = TRUE, $update = TRUE, $delete = TRUE)
+	{
+		if($controller == NULL)
+		{
+			$controller = ucfirst($route);
+
+			if(stringEndsWith($controller, 's') == TRUE)
+			{
+				$controller = substr($controller, 0, -1);
+			}
+
+			if(stringEndsWith($controller, 'ie') == TRUE)
+			{
+				$controller = substr($controller, 0, -2) . 'y';
+			}
+		}
+
+		$path = "App\\API\\" . strtoupper($version) . "\\Controllers\\" . $controller . "Controller@";
+
+		if($getList == TRUE)
+		{
+			$api->get($route, $path . 'getList');
+		}
+
+		if($get == TRUE)
+		{
+			$api->get($route . '/{id}', $path . 'get');
+		}
+
+		if($create == TRUE)
+		{
+			$api->post($route, $path . 'create');
+		}
+
+		if($update == TRUE)
+		{
+			$api->post($route . '/{id}', $path . 'update');
+		}
+
+		if($delete == TRUE)
+		{
+			$api->delete($route . '/{id}', $path . 'delete');
+		}
+	}
+}
+
+if(function_exists('stringEndsWith') == FALSE)
+{
+	/**
+	 * @param $haystack
+	 * @param $needle
+	 *
+	 * @return bool
+	 * @link http://stackoverflow.com/a/10473026
+	 */
+	function stringEndsWith($haystack, $needle)
+	{
+		// search forward starting from end minus needle length characters
+		return $needle === "" || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $temp) !== FALSE);
+	}
+}
