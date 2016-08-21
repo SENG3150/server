e1022ade1da6538d547f0167651f7e0dd5e57e68
Mitchell Davis <mitchell.peter.davis@gmail.com>
2016-08-21 22:49:18 +1000

    Updated parser to work properly.

--- app/API/V1/Parsers/DowntimeParser.php
+++ app/API/V1/Parsers/DowntimeParser.php
@@ -6,41 +6,67 @@ use App\API\V1\Repositories\DowntimeRepository as Repository;
 use Illuminate\Http\Request;
 use App;
 
-class DowntimeParser extends Parser {
-
-    /** @var Repository $repository */
-    var $repository;
-
-    public function create($input, $recursive = TRUE)
-    {
-        $input = $this->resolveInput($input);
-
-        $this->validateArray(
-            $input,
-            array(
-                'downTimeHours'          => 'required',
-                'reason' => 'required|',
-            )
-        );
-        $machineType=null;
-        $machine = $this->resolveOne(
-            $input,
-            array(
-                'machine' => App\API\V1\Repositories\MachineRepository::class,
-
-            ), $machineType
-        );
-        if($machineType == NULL)
-        {
-            throw new \Dingo\Api\Exception\ValidationHttpException(
-                array(
-                    'machine' => 'No machine was set.'
-                )
-            );
-        }
-        $entity = new Entity();
-        $this->resolve($entity, $input, 'reason');
-        $this->resolve($entity, $input, 'downTimeHours');
-        $entity->setMachine($machine);
-    }
+class DowntimeParser extends Parser
+{
+	/** @var Repository $repository */
+	var $repository;
+	
+	public function create($input, $recursive = TRUE)
+	{
+		$input = $this->resolveInput($input);
+		
+		$this->validateArray(
+			$input,
+			array(
+				'machine'       => 'required',
+				'systemName'    => 'required',
+				'downTimeHours' => 'required',
+			)
+		);
+		
+		$entity = new Entity();
+		
+		$this->resolve($entity, $input, 'machine', 'entity', App\API\V1\Repositories\DowntimeRepository::class);
+		$this->resolve($entity, $input, 'systemName');
+		$this->resolve($entity, $input, 'downTimeHours', 'double');
+		$this->resolve($entity, $input, 'reason');
+		
+		$this->em->persist($entity);
+		$this->em->flush();
+		
+		return $entity;
+	}
+	
+	/**
+	 * @param Request|array $input
+	 * @param int           $id
+	 * @param bool          $recursive
+	 *
+	 * @return Entity
+	 */
+	public function update($input, $id, $recursive = TRUE)
+	{
+		/** @var Entity $entity */
+		$entity = $this->repository->find($id);
+		
+		if($entity != NULL)
+		{
+			$input = $this->resolveInput($input);
+			
+			$this->resolve($entity, $input, 'machine', 'entity', App\API\V1\Repositories\DowntimeRepository::class);
+			$this->resolve($entity, $input, 'systemName');
+			$this->resolve($entity, $input, 'downTimeHours', 'double');
+			$this->resolve($entity, $input, 'reason');
+			
+			$this->em->persist($entity);
+			$this->em->flush();
+			
+			return $entity;
+		}
+		
+		else
+		{
+			throw new \Symfony\Component\HttpKernel\Exception\NotFoundHttpException();
+		}
+	}
 }
