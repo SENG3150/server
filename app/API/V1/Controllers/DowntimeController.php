2f7e38ebba759f5017645268d0d5860f8a527e5b
Mitchell Davis <mitchell.peter.davis@gmail.com>
2016-08-26 14:03:18 +1000

    Resolved unresolved Downtime reference.

--- app/API/V1/Controllers/DowntimeController.php
+++ app/API/V1/Controllers/DowntimeController.php
@@ -2,6 +2,7 @@
 
 namespace App\API\V1\Controllers;
 
+use App\API\V1\Entities\Downtime as Entity;
 use App\API\V1\Parsers\DowntimeParser as Parser;
 use App\API\V1\Repositories\DowntimeRepository as Repository;
 use App\API\V1\Transformers\DowntimeTransformer as Transformer;
@@ -14,39 +15,42 @@ use App\API\V1\Repositories\MachineRepository;
 
 class  DowntimeController extends APIController
 {
-    public function getList(Repository $repository)
-    {
-        $entities = $repository->findAll();
-
-        return $this->response->collection(Collection::make($entities), new Transformer());
-    }
-
-    public function get($id, Repository $repository)
-    {
-        $entity = $repository->find($id);
-
-        if ($entity != NULL) {
-            return $this->response->item($entity, new Transformer());
-        } else {
-            $this->response()->errorNotFound();
-
-            return FALSE;
-        }
-    }
-
-    public function create(Request $request, Parser $parser)
-    {
-        $parser->handle($request);
-
-        return $this->response()->created();
-    }
-
-    public function update($id, Request $request, Parser $parser)
-    {
-        $parser->handle($request, $id);
-
-        return $this->response()->accepted();
-    }
+	public function getList(Repository $repository)
+	{
+		$entities = $repository->findAll();
+		
+		return $this->response->collection(Collection::make($entities), new Transformer());
+	}
+	
+	public function get($id, Repository $repository)
+	{
+		$entity = $repository->find($id);
+		
+		if($entity != NULL)
+		{
+			return $this->response->item($entity, new Transformer());
+		}
+		else
+		{
+			$this->response()->errorNotFound();
+			
+			return FALSE;
+		}
+	}
+	
+	public function create(Request $request, Parser $parser)
+	{
+		$parser->handle($request);
+		
+		return $this->response()->created();
+	}
+	
+	public function update($id, Request $request, Parser $parser)
+	{
+		$parser->handle($request, $id);
+		
+		return $this->response()->accepted();
+	}
 	
 	public function delete($id, Repository $repository, EntityManagerInterface $em)
 	{
@@ -79,35 +83,40 @@ class  DowntimeController extends APIController
 			return FALSE;
 		}
 	}
-
-    public function machine($id, Repository $repository, MachineRepository $machineRepository)
-    {
-        $machine = $machineRepository->find($id);
-
-        if ($machine != NULL) {
-            $items = $repository->findByMachine($machine);
-
-            return $this->response->collection(Collection::make($items), new Transformer());
-        } else {
-            $this->response()->errorNotFound();
-
-            return FALSE;
-        }
-    }
-
-    public function createBulk(Request $request, Parser $parser)
-    {
-        $input = $request->input();
-
-        if (array_key_exists('data', $input) == TRUE) {
-            foreach ($input['data'] as $item) {
-                $item['machine'] = $input['machine'];
-
-                /** @var Downtime $downtimeEntity */
-                $downtimeEntity = $parser->handle($item);
-            }
-        }
-
-        return $this->response->created();
-    }
+	
+	public function machine($id, Repository $repository, MachineRepository $machineRepository)
+	{
+		$machine = $machineRepository->find($id);
+		
+		if($machine != NULL)
+		{
+			$items = $repository->findByMachine($machine);
+			
+			return $this->response->collection(Collection::make($items), new Transformer());
+		}
+		else
+		{
+			$this->response()->errorNotFound();
+			
+			return FALSE;
+		}
+	}
+	
+	public function createBulk(Request $request, Parser $parser)
+	{
+		$input = $request->input();
+		
+		if(array_key_exists('data', $input) == TRUE)
+		{
+			foreach($input['data'] as $item)
+			{
+				$item['machine'] = $input['machine'];
+				
+				/** @var Entity $entity */
+				$entity = $parser->handle($item);
+			}
+		}
+		
+		return $this->response->created();
+	}
 }
