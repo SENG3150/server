0651a0e6a9b2b8010d7454182dc6beca8b6a7ebb
Mitchell Davis <mitchell.peter.davis@gmail.com>
2016-08-31 15:34:42 +1000

    Fixes to bulk.

--- app/API/V1/Controllers/InspectionController.php
+++ app/API/V1/Controllers/InspectionController.php
@@ -112,16 +112,29 @@ class InspectionController extends APIController
 	
 	public function bulk(Request $request, Repository $repository, Parser $parser, InspectionMajorAssemblyParser $inspectionMajorAssemblyParser, InspectionSubAssemblyParser $inspectionSubAssemblyParser, InspectionMajorAssemblyRepository $inspectionMajorAssemblyRepository, InspectionSubAssemblyRepository $inspectionSubAssemblyRepository, CommentParser $commentParser, MachineGeneralTestParser $machineGeneralTestParser, OilTestParser $oilTestParser, WearTestParser $wearTestParser, PhotoParser $photoParser, ActionItemParser $actionItemParser, EntityManagerInterface $em)
 	{
-		$technician = $this->getUser()->getTechnician();
-		
-		if($technician == NULL)
-		{
-			throw new \Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException('You must be a technician to use this endpoint.');
-		}
-		
 		$input = $request->input();
 		
-		$input['technician'] = $technician->getId();
+		if($this->getUser()->isDomainExpert() == TRUE || $this->getUser()->isTechnician() == TRUE)
+		{
+			if($this->getUser()->isDomainExpert() == TRUE)
+			{
+				$domainExpert = $this->getUser()->getDomainExpert();
+				
+				$input['domainExpert'] = $domainExpert->getId();
+			}
+			
+			else if($this->getUser()->isTechnician() == TRUE)
+			{
+				$technician = $this->getUser()->getTechnician();
+				
+				$input['technician'] = $technician->getId();
+			}
+		}
+		
+		else
+		{
+			throw new \Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException('You must be a domain expert or technician to use this endpoint.');
+		}
 		
 		/** @var Inspection $entity */
 		$entity = $parser->handle($input);
@@ -165,227 +178,232 @@ class InspectionController extends APIController
 			$entity = $repository->find($entity->getId());
 		}
 		
-		if(array_key_exists('comments', $input) == TRUE)
+		if($this->getUser()->isTechnician() == TRUE)
 		{
-			foreach($input['comments'] as $comment)
+			$technician = $this->getUser()->getTechnician();
+			
+			if(array_key_exists('comments', $input) == TRUE)
 			{
-				$comment['inspection'] = $entity->getId();
-				$comment['technician'] = $technician->getId();
-				
-				$commentEntity = $commentParser->handle($comment);
-			}
-		}
-		
-		if(array_key_exists('photos', $input) == TRUE)
-		{
-			foreach($input['photos'] as $photo)
-			{
-				$photo['inspection'] = $entity->getId();
-				$photo['technician'] = $technician->getId();
-				
-				$photoEntity = $photoParser->handle($photo);
-			}
-		}
-		
-		if(array_key_exists('majorAssemblies', $input) == TRUE)
-		{
-			foreach($input['majorAssemblies'] as $majorAssembly)
-			{
-				$majorAssemblyEntity = $inspectionMajorAssemblyRepository->find($majorAssembly['id']);
-				
-				if($majorAssemblyEntity != NULL)
+				foreach($input['comments'] as $comment)
 				{
-					if(array_key_exists('comments', $majorAssembly) == TRUE)
-					{
-						foreach($majorAssembly['comments'] as $comment)
-						{
-							$comment['majorAssembly'] = $majorAssemblyEntity->getId();
-							$comment['technician']    = $technician->getId();
-							
-							$commentEntity = $commentParser->handle($comment);
-						}
-					}
+					$comment['inspection'] = $entity->getId();
+					$comment['technician'] = $technician->getId();
 					
-					if(array_key_exists('photos', $majorAssembly) == TRUE)
-					{
-						foreach($majorAssembly['photos'] as $photo)
-						{
-							$photo['majorAssembly'] = $majorAssemblyEntity->getId();
-							$photo['technician']    = $technician->getId();
-							
-							$photoEntity = $photoParser->handle($photo);
-						}
-					}
-					
-					if(array_key_exists('subAssemblies', $majorAssembly) == TRUE)
-					{
-						foreach($majorAssembly['subAssemblies'] as $subAssembly)
-						{
-							$subAssemblyEntity = $inspectionSubAssemblyRepository->find($subAssembly['id']);
-							
-							if($subAssemblyEntity != NULL)
-							{
-								if(array_key_exists('comments', $subAssembly) == TRUE)
-								{
-									foreach($subAssembly['comments'] as $comment)
-									{
-										$comment['subAssembly'] = $subAssemblyEntity->getId();
-										$comment['technician']  = $technician->getId();
-										
-										$commentEntity = $commentParser->handle($comment);
-									}
-								}
-								
-								if(array_key_exists('photos', $subAssembly) == TRUE)
-								{
-									foreach($subAssembly['photos'] as $photo)
-									{
-										$photo['subAssembly'] = $subAssemblyEntity->getId();
-										$photo['technician']  = $technician->getId();
-										
-										$photoEntity = $photoParser->handle($photo);
-									}
-								}
-								
-								if(array_key_exists('machineGeneralTest', $subAssembly) == TRUE && $subAssembly['machineGeneralTest'] != NULL)
-								{
-									$machineGeneralTest = $subAssembly['machineGeneralTest'];
-									
-									$machineGeneralTest['inspection']  = $entity->getId();
-									$machineGeneralTest['subAssembly'] = $subAssemblyEntity->getId();
-									
-									/** @var MachineGeneralTest $machineGeneralTestEntity */
-									$machineGeneralTestEntity = $machineGeneralTestParser->handle($machineGeneralTest);
-									
-									if(array_key_exists('comments', $machineGeneralTest) == TRUE)
-									{
-										foreach($machineGeneralTest['comments'] as $comment)
-										{
-											$comment['machineGeneralTest'] = $machineGeneralTestEntity->getId();
-											$comment['technician']         = $technician->getId();
-											
-											$commentEntity = $commentParser->handle($comment);
-										}
-									}
-									
-									if(array_key_exists('photos', $machineGeneralTest) == TRUE)
-									{
-										foreach($machineGeneralTest['photos'] as $photo)
-										{
-											$photo['machineGeneralTest'] = $machineGeneralTestEntity->getId();
-											$photo['technician']         = $technician->getId();
-											
-											$photoEntity = $photoParser->handle($photo);
-										}
-									}
-									
-									if(array_key_exists('actionItem', $machineGeneralTest) == TRUE && $machineGeneralTest['actionItem'] != NULL)
-									{
-										$actionItem = $machineGeneralTest['actionItem'];
-										
-										$actionItem['machineGeneralTest'] = $machineGeneralTestEntity->getId();
-										$actionItem['technician'] = $technician->getId();
-										
-										$actionItemEntity = $actionItemParser->handle($actionItem);
-									}
-								}
-								
-								if(array_key_exists('oilTest', $subAssembly) == TRUE && $subAssembly['oilTest'] != NULL)
-								{
-									$oilTest = $subAssembly['oilTest'];
-									
-									$oilTest['inspection']  = $entity->getId();
-									$oilTest['subAssembly'] = $subAssemblyEntity->getId();
-									
-									/** @var OilTest $oilTestEntity */
-									$oilTestEntity = $oilTestParser->handle($oilTest);
-									
-									if(array_key_exists('comments', $oilTest) == TRUE)
-									{
-										foreach($oilTest['comments'] as $comment)
-										{
-											$comment['oilTest']    = $oilTestEntity->getId();
-											$comment['technician'] = $technician->getId();
-											
-											$commentEntity = $commentParser->handle($comment);
-										}
-									}
-									
-									if(array_key_exists('photos', $oilTest) == TRUE)
-									{
-										foreach($oilTest['photos'] as $photo)
-										{
-											$photo['oilTest']    = $oilTestEntity->getId();
-											$photo['technician'] = $technician->getId();
-											
-											$photoEntity = $photoParser->handle($photo);
-										}
-									}
-									
-									if(array_key_exists('actionItem', $oilTest) == TRUE && $oilTest['actionItem'] != NULL)
-									{
-										$actionItem = $oilTest['actionItem'];
-										
-										$actionItem['oilTest'] = $oilTestEntity->getId();
-										$actionItem['technician'] = $technician->getId();
-										
-										$actionItemEntity = $actionItemParser->handle($actionItem);
-									}
-								}
-								
-								if(array_key_exists('wearTest', $subAssembly) == TRUE && $subAssembly['wearTest'] != NULL)
-								{
-									$wearTest = $subAssembly['wearTest'];
-									
-									$wearTest['inspection']  = $entity->getId();
-									$wearTest['subAssembly'] = $subAssemblyEntity->getId();
-									
-									/** @var WearTest $wearTestEntity */
-									$wearTestEntity = $wearTestParser->handle($wearTest);
-									
-									if(array_key_exists('comments', $wearTest) == TRUE)
-									{
-										foreach($wearTest['comments'] as $comment)
-										{
-											$comment['wearTest']   = $wearTestEntity->getId();
-											$comment['technician'] = $technician->getId();
-											
-											$commentEntity = $commentParser->handle($comment);
-										}
-									}
-									
-									if(array_key_exists('photos', $wearTest) == TRUE)
-									{
-										foreach($wearTest['photos'] as $photo)
-										{
-											$photo['wearTest']   = $wearTestEntity->getId();
-											$photo['technician'] = $technician->getId();
-											
-											$photoEntity = $photoParser->handle($photo);
-										}
-									}
-									
-									if(array_key_exists('actionItem', $wearTest) == TRUE && $wearTest['actionItem'] != NULL)
-									{
-										$actionItem = $wearTest['actionItem'];
-										
-										$actionItem['wearTest'] = $wearTestEntity->getId();
-										$actionItem['technician'] = $technician->getId();
-										
-										$actionItemEntity = $actionItemParser->handle($actionItem);
-									}
-								}
-							}
-							
-							else
-							{
-							}
-						}
-					}
+					$commentEntity = $commentParser->handle($comment);
 				}
-				
-				else
+			}
+			
+			if(array_key_exists('photos', $input) == TRUE)
+			{
+				foreach($input['photos'] as $photo)
 				{
+					$photo['inspection'] = $entity->getId();
+					$photo['technician'] = $technician->getId();
+					
+					$photoEntity = $photoParser->handle($photo);
+				}
+			}
+			
+			if(array_key_exists('majorAssemblies', $input) == TRUE)
+			{
+				foreach($input['majorAssemblies'] as $majorAssembly)
+				{
+					$majorAssemblyEntity = $inspectionMajorAssemblyRepository->find($majorAssembly['id']);
+					
+					if($majorAssemblyEntity != NULL)
+					{
+						if(array_key_exists('comments', $majorAssembly) == TRUE)
+						{
+							foreach($majorAssembly['comments'] as $comment)
+							{
+								$comment['majorAssembly'] = $majorAssemblyEntity->getId();
+								$comment['technician']    = $technician->getId();
+								
+								$commentEntity = $commentParser->handle($comment);
+							}
+						}
+						
+						if(array_key_exists('photos', $majorAssembly) == TRUE)
+						{
+							foreach($majorAssembly['photos'] as $photo)
+							{
+								$photo['majorAssembly'] = $majorAssemblyEntity->getId();
+								$photo['technician']    = $technician->getId();
+								
+								$photoEntity = $photoParser->handle($photo);
+							}
+						}
+						
+						if(array_key_exists('subAssemblies', $majorAssembly) == TRUE)
+						{
+							foreach($majorAssembly['subAssemblies'] as $subAssembly)
+							{
+								$subAssemblyEntity = $inspectionSubAssemblyRepository->find($subAssembly['id']);
+								
+								if($subAssemblyEntity != NULL)
+								{
+									if(array_key_exists('comments', $subAssembly) == TRUE)
+									{
+										foreach($subAssembly['comments'] as $comment)
+										{
+											$comment['subAssembly'] = $subAssemblyEntity->getId();
+											$comment['technician']  = $technician->getId();
+											
+											$commentEntity = $commentParser->handle($comment);
+										}
+									}
+									
+									if(array_key_exists('photos', $subAssembly) == TRUE)
+									{
+										foreach($subAssembly['photos'] as $photo)
+										{
+											$photo['subAssembly'] = $subAssemblyEntity->getId();
+											$photo['technician']  = $technician->getId();
+											
+											$photoEntity = $photoParser->handle($photo);
+										}
+									}
+									
+									if(array_key_exists('machineGeneralTest', $subAssembly) == TRUE && $subAssembly['machineGeneralTest'] != NULL)
+									{
+										$machineGeneralTest = $subAssembly['machineGeneralTest'];
+										
+										$machineGeneralTest['inspection']  = $entity->getId();
+										$machineGeneralTest['subAssembly'] = $subAssemblyEntity->getId();
+										
+										/** @var MachineGeneralTest $machineGeneralTestEntity */
+										$machineGeneralTestEntity = $machineGeneralTestParser->handle($machineGeneralTest);
+										
+										if(array_key_exists('comments', $machineGeneralTest) == TRUE)
+										{
+											foreach($machineGeneralTest['comments'] as $comment)
+											{
+												$comment['machineGeneralTest'] = $machineGeneralTestEntity->getId();
+												$comment['technician']         = $technician->getId();
+												
+												$commentEntity = $commentParser->handle($comment);
+											}
+										}
+										
+										if(array_key_exists('photos', $machineGeneralTest) == TRUE)
+										{
+											foreach($machineGeneralTest['photos'] as $photo)
+											{
+												$photo['machineGeneralTest'] = $machineGeneralTestEntity->getId();
+												$photo['technician']         = $technician->getId();
+												
+												$photoEntity = $photoParser->handle($photo);
+											}
+										}
+										
+										if(array_key_exists('actionItem', $machineGeneralTest) == TRUE && $machineGeneralTest['actionItem'] != NULL)
+										{
+											$actionItem = $machineGeneralTest['actionItem'];
+											
+											$actionItem['machineGeneralTest'] = $machineGeneralTestEntity->getId();
+											$actionItem['technician']         = $technician->getId();
+											
+											$actionItemEntity = $actionItemParser->handle($actionItem);
+										}
+									}
+									
+									if(array_key_exists('oilTest', $subAssembly) == TRUE && $subAssembly['oilTest'] != NULL)
+									{
+										$oilTest = $subAssembly['oilTest'];
+										
+										$oilTest['inspection']  = $entity->getId();
+										$oilTest['subAssembly'] = $subAssemblyEntity->getId();
+										
+										/** @var OilTest $oilTestEntity */
+										$oilTestEntity = $oilTestParser->handle($oilTest);
+										
+										if(array_key_exists('comments', $oilTest) == TRUE)
+										{
+											foreach($oilTest['comments'] as $comment)
+											{
+												$comment['oilTest']    = $oilTestEntity->getId();
+												$comment['technician'] = $technician->getId();
+												
+												$commentEntity = $commentParser->handle($comment);
+											}
+										}
+										
+										if(array_key_exists('photos', $oilTest) == TRUE)
+										{
+											foreach($oilTest['photos'] as $photo)
+											{
+												$photo['oilTest']    = $oilTestEntity->getId();
+												$photo['technician'] = $technician->getId();
+												
+												$photoEntity = $photoParser->handle($photo);
+											}
+										}
+										
+										if(array_key_exists('actionItem', $oilTest) == TRUE && $oilTest['actionItem'] != NULL)
+										{
+											$actionItem = $oilTest['actionItem'];
+											
+											$actionItem['oilTest']    = $oilTestEntity->getId();
+											$actionItem['technician'] = $technician->getId();
+											
+											$actionItemEntity = $actionItemParser->handle($actionItem);
+										}
+									}
+									
+									if(array_key_exists('wearTest', $subAssembly) == TRUE && $subAssembly['wearTest'] != NULL)
+									{
+										$wearTest = $subAssembly['wearTest'];
+										
+										$wearTest['inspection']  = $entity->getId();
+										$wearTest['subAssembly'] = $subAssemblyEntity->getId();
+										
+										/** @var WearTest $wearTestEntity */
+										$wearTestEntity = $wearTestParser->handle($wearTest);
+										
+										if(array_key_exists('comments', $wearTest) == TRUE)
+										{
+											foreach($wearTest['comments'] as $comment)
+											{
+												$comment['wearTest']   = $wearTestEntity->getId();
+												$comment['technician'] = $technician->getId();
+												
+												$commentEntity = $commentParser->handle($comment);
+											}
+										}
+										
+										if(array_key_exists('photos', $wearTest) == TRUE)
+										{
+											foreach($wearTest['photos'] as $photo)
+											{
+												$photo['wearTest']   = $wearTestEntity->getId();
+												$photo['technician'] = $technician->getId();
+												
+												$photoEntity = $photoParser->handle($photo);
+											}
+										}
+										
+										if(array_key_exists('actionItem', $wearTest) == TRUE && $wearTest['actionItem'] != NULL)
+										{
+											$actionItem = $wearTest['actionItem'];
+											
+											$actionItem['wearTest']   = $wearTestEntity->getId();
+											$actionItem['technician'] = $technician->getId();
+											
+											$actionItemEntity = $actionItemParser->handle($actionItem);
+										}
+									}
+								}
+								
+								else
+								{
+								}
+							}
+						}
+					}
+					
+					else
+					{
+					}
 				}
 			}
 		}
